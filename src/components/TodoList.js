7120c0d37240922dcb80369874865d1eef0995a8
Mahfuz Moyeen <96814940+mahfuz-moyeen@users.noreply.github.com>
2022-08-24 01:48:21 +0600

    add todo handle

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Todo from './Todo';
 
 const TodoList = () => {
+    const todos = useSelector((state) => state.todo);
     return (
         <div>
-                    <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-            <Todo />
-        </div>
+            <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+                {
+                    todos.map((todo) => <Todo key={todo.id} todo={todo} />)
+                }
+
+            </div>
         </div>
     );
 };
