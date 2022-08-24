5fd9e23a9644766715d53e8df1f1f176a063f617
Mahfuz Moyeen <96814940+mahfuz-moyeen@users.noreply.github.com>
2022-08-24 19:11:26 +0600

    filter data

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,11 +4,33 @@ import Todo from './Todo';
 
 const TodoList = () => {
     const todos = useSelector((state) => state.todo);
+    const { status, colors } = useSelector(state => state.filter);
+
+    const checkStatus = (todo) => {
+        switch (status) {
+            case "Incomplete":
+                return !todo.completed;
+
+            case "Complete":
+                return todo.completed;
+
+            default:
+                return true;
+        }
+    };
+
+    const checkColor = (todo) => {
+        if (colors.length > 0) {
+            return colors.includes(todo?.color)
+        }
+        return true;
+    };
+
     return (
         <div>
             <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
                 {
-                    todos.map((todo) => <Todo key={todo.id} todo={todo} />)
+                    todos.filter(checkStatus).filter(checkColor).map((todo) => <Todo key={todo.id} todo={todo} />)
                 }
 
             </div>
